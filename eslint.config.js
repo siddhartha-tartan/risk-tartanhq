4d162c8662c4ec47c69df6ec9982153641583eef
siddhartha <siddhartha.anand@tartanhq.com>
2025-04-24 16:52:47 +0530

    Fix: Update ESLint configuration and package.json with type: module

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 import js from '@eslint/js';
 import tsParser from '@typescript-eslint/parser';
+import nextjs from 'eslint-config-next';
 
 export default [
   js.configs.recommended,
+  nextjs,
   {
     languageOptions: {
       parser: tsParser,
